d1efda041f295cf381e6c513944856cebc9ee9e2
Antony Lewis <cmbant@users.noreply.github.com>
2025-06-16 16:01:09 +0000

    Fix build error: Add tree.h include before fold-const.h
    The fold-const.h header requires tree.h to be included first to define
    the tree_code enum. This fixes the compilation error:
    
    error: 'tree_code' does not name a type

--- gcc/fortran/class.cc
+++ gcc/fortran/class.cc
@@ -59,6 +59,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "config.h"
 #include "system.h"
 #include "coretypes.h"
+#include "tree.h"
 #include "gfortran.h"
 #include "constructor.h"
 #include "target-memory.h"
